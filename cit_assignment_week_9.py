b9c415f2c6e82856fc695b6ba740c20cc8e8e137
Samuel Okellogum <oksam21@gmail.com>
2022-10-17 18:01:43 +0300

    adds assignment

--- cit_assignment_week_9.py
+++ cit_assignment_week_9.py
@@ -12,14 +12,48 @@ def get_hacker_new():
 
 
     # getting text
-    title = first.find_next_sibling('tr').find('span', class_ = 'score')
+    
 
-    if title:
-        title = title.text
-    else:
-        title = "0 points"
+    
+    for f in first:
+        title = f.find_next_sibling('tr').find('span', class_ = 'score')
+        comments_ = f.find_next_sibling('tr').find_all('a')[-1].text
+        author_ = f.find_next_sibling('tr').find('a', class_ = 'hnuser').text
+        rank_ = f.find('span', class_ = 'rank').text.strip('.')
 
-    author_ = first.find_next_sibling('tr').find('a', class_ = 'hnuser').text
-    rank_ = first.find('span', class_ = 'rank').text.split('.')[0]
+        if title:
+            title = title.text
+        else:
+            title = "0 points"   
 
-get_hacker_new()
+        if author_:
+            author_ = author_.text
+        else:
+            author_ = "No author"
+
+        data.append({
+            'title': title,
+            'link': link,
+            'score': title,
+            'comments':"{} comments" .format(comments_.split('')),
+            'author': author_,
+            'rank': rank_
+
+        })
+
+        return data
+
+def csv_save(data, filename):
+    with open(filename, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(data[0].keys())
+        for row in data:
+            writer.writerow(row.values())
+
+def main():
+    hacker_news = get_hacker_new()
+    csv_save(hacker_news, 'hacker_news.csv')
+
+
+if __name__ == '__main__':
+    main()
