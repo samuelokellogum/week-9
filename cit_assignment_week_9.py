a6d9ed65f5f705ed7c003da6144dc6c1f9d6e728
Samuel Okellogum <oksam21@gmail.com>
2022-10-17 17:15:56 +0300

    adds assignment

--- cit_assignment_week_9.py
+++ cit_assignment_week_9.py
@@ -7,4 +7,16 @@ def get_hacker_new():
     response = requests.get(url)
 
     soup = BeautifulSoup(response.text, 'html.parser')
-    
+
+    first = soup.find('tr', class_ = 'athing')
+
+
+    # getting text
+    title = first.find_next_sibling('tr').find('span', class_ = 'score')
+
+    if title:
+        title = title.text
+    else:
+        title = "0 points"
+
+get_hacker_new()
